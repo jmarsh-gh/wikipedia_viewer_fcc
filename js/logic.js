fa836dc54f56633e7da31d5d49ce6bd42858b739
jmarsh <j.marshmoto@gmail.com>
2017-01-15 18:36:09 +0000

    Day 13 #100Days

--- js/logic.js
+++ js/logic.js
@@ -1,17 +1,42 @@
-console.log("linked");
-$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=bees&callback=?", function(json) {	
-	console.log(json.query.search[0].title);
-	// document.querySelector("#test").innerHTML = json.query.search[0].title + ": " + json.query.search[0].snippet;		
-});	
+function main(){
+	console.log("linked");
+	//Get search results
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=bees&callback=?", function(json) {	
+		console.log(json.query.search[0].title);
+		
+		
+		//From search results get Article extracts by title
+		//Loop through search results above for top 5
+		for(var x = 0; x < 5; x++){
+			//GET title of each search result in array and build URL
+			var title = json.query.search[x].title;
+			console.log(title);
+			//add indexpageids to URL. Shows page ID for easier retrieval of extract. Look at API endpoint in browser to see.
+			var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&indexpageids&titles=" + title + "&callback=?";
+			//GET article extract by title for each search result
+			$.getJSON(url, function(json) {	
+				var extract = json.query.pages[json.query.pageids[0]].extract;
+				var articleTitle = json.query.pages[json.query.pageids[0]].title;
+				console.log(articleTitle);
+				console.log(extract);
+				var list = document.querySelector("#list");
+				//Make this slice at indexOf the first period (so one sentence)
+				//Make it only slice at word boundary period
+				list.appendChild(document.createElement('li')).textContent=articleTitle + ": " + extract.substring(0, extract.indexOf(".") + 1);
 
-$.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&indexpageids&titles=Bee&callback=?", function(json) {	
-	//add indexpageids to URL so can access the right page
-	console.log(json.query.pageids);
-	console.log(json.query.pages[json.query.pageids[0]].title);
-	var extract = json.query.pages[json.query.pageids[0]].extract.substring(0, 140);
-	console.log(extract);
-	document.querySelector("#test").innerHTML = json.query.pages[json.query.pageids[0]].title + ": " + extract;
+			}); //Article GET
+		}; //FOR IN
+	});	// Search Result GET
 
 
-			
-});		
+	var random = document.querySelector("button");
+	random.addEventListener("click", function(){
+	});
+
+};
+
+$(document).ready(main());
+
+//document.querySelector("#test").innerHTML = json.query.pages[json.query.pageids[0]].title + ": " + extract;
+//https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&indexpageids&titles=Stack%20Overflow
+//https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=bees
--- todo.md
+++ todo.md
@@ -1,8 +1,13 @@
 Get Basic Data from Wikipedia - done
-List entry for top 5 search results
-append each search result + extract to the unordered list
-Shorten extract. Index of second period? To get full sentences.
+List entry for top 5 search results - done
+append each search result + extract to the unordered list - done
+Shorten extract. Index of second period? To get full sentences. - done
+Random article - https://en.wikipedia.org/wiki/Special:Random - done
+Search Field - Done
 Find a way to get extract in srsearch to avoid second getJSON
+If keeping second JSON investigate the random order
+indexOf full stop at end of sentence rather than just any period
+link first GET (srs search) to the input field.
+Concat search URL with user input value
+Make list items into link to the articles
 
-Search Field
-Concat search URL with user input value/.
