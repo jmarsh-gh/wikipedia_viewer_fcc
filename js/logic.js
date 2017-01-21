f25165d2a9d9af53cf9e058540dddea6ff34d03c
jmarsh <j.marshmoto@gmail.com>
2017-01-21 19:16:10 +0000

    Day 19 #100Days

--- js/logic.js
+++ js/logic.js
@@ -4,44 +4,64 @@ function main(){
 	//onclick search button
 	var search = document.querySelector("#search");
 	search.addEventListener("click", function(){
-		//reset any previous results
-		document.querySelector("#list").innerHTML = "";
-		//Get search results
-		//search = input value
-		var input = document.querySelector("#searchText");
-		var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + input.value + "&callback=?";
-		$.getJSON(searchUrl, function(json) {	
-			console.log(json.query.search[0].title);
-			
-			//From search results get Article extracts by title
-			//Loop through search results above for top 5
-			for(var x = 0; x < 5; x++){
-				//GET title of each search result in array and build URL
-				var title = json.query.search[x].title;
-				console.log(title);
-				//add indexpageids to URL. Shows page ID for easier retrieval of extract. Look at API endpoint in browser to see.
-				var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&indexpageids&titles=" + title + "&callback=?";
-				//GET article extract by title for each search result
-				$.getJSON(url, function(json) {	
-					var extract = json.query.pages[json.query.pageids[0]].extract;
-					//truncate extract at first period
-					// var extractTrunc = extract.substring(0, extract.indexOf(".") + 1);
-					var period = extract.search(/\B([a-z])\.\B/) //("/([a-z])\.\B/");
-					console.log(period++);
-					var extractTrunc = extract.substring(0, period + 1);
-					var articleTitle = json.query.pages[json.query.pageids[0]].title;
-					var list = document.querySelector("#list");
-					//link to this pages URL using query string: ?curid=_
-					var pageUrl = "https://en.wikipedia.org/wiki?curid=" + json.query.pageids[0];
-					console.log(pageUrl);
-					list.appendChild(document.createElement('div')).innerHTML = "<a href='" + pageUrl + "' class='list-group-item' target='_blank'>" + "<h4 class='list-group-item-heading'>" + articleTitle + "</h4>" + "<p class='list-group-item-text'>" + extractTrunc + "</p>" + "</a>"; 
-					
-					
-				}); //Article GET
-			}; //FOR IN
-		});	// Search Result GET
-		input.value = "";
+		results();
 	}); //Search Click Event
+
+	var inputEnter = document.querySelector("#searchText");
+	inputEnter.addEventListener("keydown", function(y){
+		if(y.which == 13){
+			results();
+		}; //if
+	}); //keydown
+
+function results(){
+	//reset any previous results
+	document.querySelector("#list").innerHTML = "";
+	//Get search results
+	//search = input value
+	var input = document.querySelector("#searchText");
+	var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + input.value + "&callback=?";
+	$.getJSON(searchUrl, function(json) {	
+		console.log(json.query.search[0].title);
+		
+		//From search results get Article extracts by title
+		//Loop through search results above for top 5
+		for(var x = 0; x < 5; x++){
+			//GET title of each search result in array and build URL
+			var title = json.query.search[x].title;
+			console.log(title);
+			//add indexpageids to URL. Shows page ID for easier retrieval of extract. Look at API endpoint in browser to see.
+			var url = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&indexpageids&titles=" + title + "&callback=?";
+			//GET article extract by title for each search result
+			$.getJSON(url, function(json) {	
+				var extract = json.query.pages[json.query.pageids[0]].extract;
+				console.log(extract);
+				//truncate extract at first end of sentence period
+				var period = extract.search(/\B([a-z])\.\B/);
+				var extractTrunc = ""
+				//The regex returns index of the letter before the period
+				//To slice correctly after the period, must add 2 to index position
+				//However disambig extracts don't have full sentences so return -1
+				//-1 + 2 = 1 so we will slice the first letter of the extract
+				//Better to only add the extract if there is a full sentence i.e. a regex match
+				if(period > -1){
+					extractTrunc = extract.substring(0, period + 2);
+				};
+				var articleTitle = json.query.pages[json.query.pageids[0]].title;
+				var list = document.querySelector("#list");
+				//link to this pages URL using query string: ?curid=_
+				var pageUrl = "https://en.wikipedia.org/wiki?curid=" + json.query.pageids[0];
+				console.log(pageUrl);
+				list.appendChild(document.createElement('div')).innerHTML = "<a href='" + pageUrl + "' class='list-group-item' target='_blank'>" + "<h4 class='list-group-item-heading'>" + articleTitle + "</h4>" + "<p class='list-group-item-text'>" + extractTrunc + "</p>" + "</a>"; 
+			}); //Article GET
+		}; //FOR IN
+	});	// Search Result GET
+	input.value = ""; 
+};
+
+
+
+
 }; //main()
 
 $(document).ready(main());
--- todo.md
+++ todo.md
@@ -3,12 +3,14 @@ List entry for top 5 search results - done
 append each search result + extract to the unordered list - done
 Shorten extract. Index of second period? To get full sentences. - done
 Random article - https://en.wikipedia.org/wiki/Special:Random - done
-Search Field - Done
-jQuery UI - sortable
-link first GET (srs search) to the input field.
-Make list items into link to the articles
+Search Field - done
+link first GET (srs search) to the input field - done
+Make list items into link to the articles - done
+Concat search URL with user input value - done
 Find a way to get extract in srsearch to avoid second getJSON
 If keeping second JSON investigate the random order
 indexOf full stop at end of sentence rather than just any period
-Concat search URL with user input value
+
+search on enter keypress
+truncate extract at second period
 
